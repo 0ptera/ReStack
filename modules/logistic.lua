d2ca8ebe11fcdb01db7a6968b74918a07d049821
Optera <yousei.kyoko+github@gmail.com>
2018-01-05 21:33:10 +0100

    0.3.9
        - added electric poles    
        - added arithmetic, decider and constant combinator
        - added red & green wire

--- modules/logistic.lua
+++ modules/logistic.lua
@@ -10,8 +10,17 @@ SelectItemByEntity("pipe-to-ground", settings.startup["ReStack-pipe"].value)
 
 SelectItemByEntity("inserter", settings.startup["ReStack-inserter"].value)
 
+SelectItemByEntity("electric-pole", settings.startup["ReStack-electric-pole"].value)
+
 SelectItemByEntity("roboport", settings.startup["ReStack-roboport"].value)
 
 for _, rail in pairs(data.raw["rail-planner"]) do
   ReStack_Items[rail.name] = {stack_size = settings.startup["ReStack-rail"].value, type = "rail"} -- inefficient but looks better in log
 end
+
+SelectItemByEntity("arithmetic-combinator", settings.startup["ReStack-combinator"].value)
+SelectItemByEntity("decider-combinator", settings.startup["ReStack-combinator"].value)
+SelectItemByEntity("constant-combinator", settings.startup["ReStack-combinator"].value)
+
+ReStack_Items["red-wire"] = {stack_size = settings.startup["ReStack-wire"].value, type = "wire"}
+ReStack_Items["green-wire"] = {stack_size = settings.startup["ReStack-wire"].value, type = "wire"}
