0323e23057718ae89808488623c8f4698af208d5
Anythingapplied <anythingapplied@userdelta.com>
2018-07-06 22:49:25 -0500

    Logic to allow other valid recipe formats
    Both ingredients and results can take either format:
    {"iron-plate", 1} or
    {type="item", name="iron-plate", amount=1}
    
    Also, a valid, but rarely used recipe format is to
    specify either .normal or .expensive, but not the other.

--- modules/lib.lua
+++ modules/lib.lua
@@ -8,7 +8,11 @@ function SelectItemByEntity(ent_type, stack_size, category)
         ReStack_Items[entity.minable.result] = {stack_size = stack_size, type = category}
       elseif entity.minable.results then
         for _, result in pairs(entity.minable.results) do
-          ReStack_Items[result.name] = {stack_size = stack_size, type = category}
+          if result.name then
+            ReStack_Items[result.name] = {stack_size = stack_size, type = category}
+          elseif result[1] then
+            ReStack_Items[result[1]] = {stack_size = stack_size, type = category}
+          end
         end
       end
     end
@@ -17,21 +21,45 @@ end
 
 -- set stacks for items by recipe
 function SelectItemsByRecipeResult(recipe, stack_size, category)
-  if recipe.result or (recipe.normal and recipe.normal.result) then
-    local item = recipe.result or recipe.normal.result
+
+  local item
+  if recipe.result then
+    item = recipe.result
+  elseif recipe.normal and recipe.normal.result then
+    item = recipe.normal.result
+  elseif recipe.expensive and recipe.expensive.result then
+    item = recipe.normal.result
+  end
+  if item then
     ReStack_Items[item] = {stack_size = stack_size, type = category}
-  elseif recipe.results or (recipe.normal and recipe.normal.results) then
-    local results = recipe.results or recipe.normal.results -- is normal.results even possible?
-    for _, result in pairs(results) do
-      ReStack_Items[result.name] = {stack_size = stack_size, type = category}
+    return
+  end
+
+  local items
+  if recipe.results then
+    items = recipe.results
+  elseif recipe.normal and recipe.normal.results then
+    items = recipe.normal.results
+  elseif recipe.expensive and recipe.expensive.results then
+    items = recipe.expensive.results
+  end
+  if items then
+    for _, result in pairs(items) do
+      if result.name then
+        ReStack_Items[result.name] = {stack_size = stack_size, type = category}
+      elseif result[1] then
+        ReStack_Items[result[1]] = {stack_size = stack_size, type = category}
+      end
     end
   end
 end
 
 function SelectItemsByRecipeInput(recipe, stack_size, category)
   if recipe.ingredients then
-    for n, ingredient in pairs(recipe.ingredients) do          
-      if ingredient[1] then
+    for n, ingredient in pairs(recipe.ingredients) do
+      if ingredient.name then
+        ReStack_Items[ingredient.name] = {stack_size = stack_size, type = category}
+      elseif ingredient[1] then
         ReStack_Items[ingredient[1]] = {stack_size = stack_size, type = category}
       end
     end
