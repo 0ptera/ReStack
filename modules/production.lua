c76b37ffe91864ad0ffd20fbe3fb3ade59933373
Optera <yousei.kyoko+github@gmail.com>
2019-08-27 15:28:47 +0200

    v0.5.7
    - added boiler types
    - added generator types (steam engine, turbine) #12

--- modules/production.lua
+++ modules/production.lua
@@ -5,14 +5,12 @@
 --]]
 
 SelectItemByEntity("reactor", settings.startup["ReStack-reactor"].value)
-
 SelectItemByEntity("assembling-machine", settings.startup["ReStack-crafting-machine"].value)
-
 SelectItemByEntity("furnace", settings.startup["ReStack-furnace"].value)
-
 SelectItemByEntity("beacon", settings.startup["ReStack-beacon"].value)
-
 SelectItemByEntity("mining-drill", settings.startup["ReStack-mining-drill"].value)
+SelectItemByEntity("boiler", settings.startup["ReStack-boiler"].value)
+SelectItemByEntity("generator", settings.startup["ReStack-generator"].value)
 
 for _, item in pairs(data.raw["module"]) do
   ReStack_Items[item.name] = {stack_size = settings.startup["ReStack-modules"].value, type = "module"}
